b4abe7973292fe4a8e1c5ba1e101668b669dee62
Nick Greiner <nicholas.greiner@hotmail.com>
2020-10-17 19:12:12 -0400

    Implemented main(), floyd(), loadMatrix(), displayMatrix(), and printPaths()

--- floyds.cpp
+++ floyds.cpp
@@ -19,11 +19,15 @@ https://github.com/NickGreiner/CSC-2710-Floyds-Program
 
 using namespace std;
 
-vector<vector<int>> floyd(vector<vector<int>> matrix);
+void floyd(vector<vector<int>> &matrix, vector<vector<int>> &returnMatrix, vector<vector<int>> &pathMatrix);
 
 vector<vector<int>> loadMatrix(string matrixFile);
-void displayMatrix(vector<vector<int>> matrix);
-// void writeMatrix(vector<vector<int>> matrix);
+void displayMatrix(vector<vector<int>> &matrix);
+void printPaths(int q, int r, vector<vector<int>> &pathMatrix);
+
+vector<vector<int>> paths;
+
+#define INFINITY 2000
 
 int main() {
   string matrixFile;
@@ -36,69 +40,129 @@ int main() {
 
   initial = loadMatrix(matrixFile);
 
-  final = floyd(initial);
+  vector<int> pathRow(initial.size(), 0);
+  vector<vector<int>> path(initial.size(), pathRow);
 
   cout << "Initial Matrix:" << endl;
   displayMatrix(initial);
 
+  floyd(initial, final, path);
+
   cout << "Final Matrix:" << endl;
   displayMatrix(final);
 
-}
-
-vector<vector<int>> floyd(vector<vector<int>> matrix) {
-  vector<vector<int>> testMatrix = matrix;
-
-  int i, j, k;
-  int n = testMatrix.size();
-
-  for (k = 1; k <= n; k++) {
-    for (i = 1; i <= n; i++) {
-      for (j = 1; j <= n; j++) {
-        testMatrix[i][j] = min(testMatrix[i][j], testMatrix[i][k] + testMatrix[k][j]);
-      }
+  if (paths.size() > 0) {
+    cout << "Paths:" << endl;
+    for (int z = 0; z < paths.size(); z++) {
+      cout << "Path from v" << paths[z][0] << " to v" << paths[z][1] << " ==> ";
+      cout << "v" << paths[z][0] << " ";
+      printPaths(paths[z][0] - 1, paths[z][1] - 1, path);
+      cout << "v" << paths[z][1];
+      cout << endl;
     }
   }
 
-  return testMatrix;
+  return 0;
+}
+
+void floyd(vector<vector<int>> &matrix, vector<vector<int>> &returnMatrix, vector<vector<int>> &pathMatrix) {
+  int i, j, k;
+  int n = matrix.size();
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      pathMatrix[i][j] = 0;
+    }
+  }
+
+  returnMatrix = matrix;
+
+  for (k = 0; k < n; k++) {
+    for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++) {
+        if (returnMatrix[i][k] + returnMatrix[k][j] < returnMatrix[i][j]) {
+          pathMatrix[i][j] = k;
+          returnMatrix[i][j] = returnMatrix[i][k] + returnMatrix[k][j];
+        }
+      }
+    }
+  }
 }
 
 vector<vector<int>> loadMatrix(string matrixFile) {
   vector<vector<int>> matrix;
 
-  ifstream ifs(matrixFile);
-  string tempstr;
-  int tempint;
+  ifstream infile(matrixFile);
 
-  int edges, vertices;
+  int vertices, edges;
 
-  ifs >> vertices;
-  ifs >> edges;
+  string v1, v2;
+  int weight;
 
-  while (getline(ifs, tempstr)) {
-    istringstream iss(tempstr);
-    vector<int> tempMatrix;
-    while (iss >> tempint) {
-      tempMatrix.push_back(tempint);
+  infile >> vertices;
+  infile >> edges;
+
+  for (int j = 0; j < vertices; j++) {
+    vector<int> matrixRow;
+    for (int k = 0; k < vertices; k++) {
+      if (j == k) {
+        matrixRow.push_back(0);
+      }
+      else {
+        matrixRow.push_back(INFINITY);
+      }
     }
-    matrix.push_back(tempMatrix);
+    matrix.push_back(matrixRow);
   }
 
-  for (auto row : matrix) {
-    for (auto el : row) {
-      cout << el << ' ';
+  for (int i = 0; i < edges; i++) {
+    infile >> v1 >> v2 >> weight;
+
+    int intv1 = v1[1] - 48;
+    int intv2 = v2[1] - 48;
+
+    matrix[intv1 - 1][intv2 - 1] = weight;
+  }
+
+  if (infile.eof()) {
+    return matrix;
+  }
+
+  else {
+    int pathsToPrint;
+
+    infile >> pathsToPrint;
+
+    for (int i = 0; i < pathsToPrint; i++) {
+      vector<int> p;
+      infile >> v1 >> v2;
+
+      int intv1 = v1[1] - 48;
+      int intv2 = v2[1] - 48;
+
+      p.push_back(intv1);
+      p.push_back(intv2);
+      paths.push_back(p);
     }
-    cout << "\n";
   }
 
   return matrix;
 }
 
-void displayMatrix(vector<vector<int>> matrix) {
-  for (auto row : matrix) {
-    for (auto el : row) {
-      cout << el << ' ';
+void displayMatrix(vector<vector<int>> &matrix) {
+  for (int i = 0; i < matrix.size(); i++) {
+    for (int j = 0; j < matrix[i].size(); j++) {
+      cout << matrix[i][j] << "\t";
     }
-    cout << "\n";
+    cout << endl;
+  }
+  cout << endl;
+}
+
+void printPaths(int q, int r, vector<vector<int>> &pathMatrix) {
+  if (pathMatrix[q][r] != 0) {
+    printPaths(q, pathMatrix[q][r], pathMatrix);
+    cout << "v" << pathMatrix[q][r] + 1 << " ";
+    printPaths(pathMatrix[q][r], r, pathMatrix);
   }
 }
